1075c7de3ebedfa85e48ca6763d2a3939b0c9f04
Vinaydg <vinaydg2001@gmail.com>
2022-09-05 07:52:49 +0530

    My first react change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      string: 'My name is'
+    }
+  }
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({string: 'My name is not'})}>Change Text</button>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
